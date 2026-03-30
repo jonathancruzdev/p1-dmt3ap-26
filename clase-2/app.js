0a9ce94567d6f61aab291a243087f4cca46ae5d3
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2026-03-30 16:39:35 -0300

    Semana 03

---
+++ clase-2/app.js
@@ -0,0 +1,8 @@
+// Variable
+let nombre = 'Sofia'; // text  | cadena | string
+let edad = 23;        // numero | Number
+let alumno = true;   // Boolean:   true (verdadero) false (falso)
+
+nombre = prompt('Ingrese su nombre');
+alert("Hola " + nombre);
+// alert( edad + 10 );
